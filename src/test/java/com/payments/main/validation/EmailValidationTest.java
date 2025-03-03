e4adf941d726cf220f223121133044553d42be39
Fernando Batista <fernandohsbatista@gmail.com>
2025-03-03 11:09:20 -0300

    test: cover EmailValidation with more tests on invalid emails

--- src/test/java/com/payments/main/validation/EmailValidationTest.java
+++ src/test/java/com/payments/main/validation/EmailValidationTest.java
@@ -2,6 +2,8 @@ package com.payments.main.validation;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -12,9 +14,34 @@ public class EmailValidationTest {
     @Test
     void shouldThrowIfEmailIsInvalid() {
         final EmailValidation sut = new EmailValidation("email");
-        final EmailInput input = new EmailInput("emailemail.com");
+        final List<String> invalidEmails = List.of(
+                "usuario@.com",
+                "usuario@com",
+                "usuario@site..com",
+                "usuario@site,com",
+                "usuario site@com",
+                "@site.com",
+                "usuario@site",
+                "usuario@site#com",
+                "usuáriô@site.com",
+                "usuario@site..com.br",
+                "usuario@@site.com",
+                "usuario@-site.com",
+                "usuario@site-.com",
+                "usuario@site_com",
+                ".usuario@site.com",
+                "usuario.@site.com",
+                "usuario@site/com",
+                "usuario@site*.com"
+        );
 
-        assertThrows(ValidationException.InvalidEmail.class, () -> sut.validate(input));
+        invalidEmails.forEach(email -> {
+            System.out.println(email);
+            assertThrows(
+                    ValidationException.InvalidEmail.class,
+                    () -> sut.validate(new EmailInput(email)));
+        });
+
     }
 
     @Test
