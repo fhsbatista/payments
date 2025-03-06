972a34e856e04da0b6047bdd0cc206a92878f96f
Fernando Batista <fernandohsbatista@gmail.com>
2025-03-06 11:56:51 -0300

    feat: ensure HttpAuthorizer returns true if api response returns it is authorized

--- src/main/java/com/payments/transactions/data/usecases/HttpAuthorizer.java
+++ src/main/java/com/payments/transactions/data/usecases/HttpAuthorizer.java
@@ -4,7 +4,10 @@ import com.payments.transactions.domain.usecases.Authorizer;
 import com.payments.transactions.domain.usecases.CreateTransactionInput;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+record ResponseModel(String status, Data data) {
+    record Data(boolean authorization) {
+    }
+}
 
 public class HttpAuthorizer implements Authorizer {
     public final static String URL = "https://util.devi.tools/api/v2/authorize";
@@ -16,7 +19,8 @@ public class HttpAuthorizer implements Authorizer {
 
     @Override
     public boolean isAuthorized(CreateTransactionInput input) {
-        restTemplate.getForEntity(URL, Map.class);
-        return false;
+        final var result = restTemplate.getForEntity(URL, ResponseModel.class);
+
+        return result.getBody().data().authorization();
     }
 }
--- src/test/java/com/payments/transactions/data/usecases/HttpAuthorizerTest.java
+++ src/test/java/com/payments/transactions/data/usecases/HttpAuthorizerTest.java
@@ -3,12 +3,14 @@ package com.payments.transactions.data.usecases;
 import com.payments.transactions.domain.usecases.CreateTransactionInput;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class HttpAuthorizerTest {
     private RestTemplate restTemplate;
@@ -20,6 +22,14 @@ class HttpAuthorizerTest {
     @BeforeEach
     void setup() {
         restTemplate = mock(RestTemplate.class);
+        mockSuccess();
+    }
+
+    void mockSuccess() {
+        final ResponseModel response = new ResponseModel("success", new ResponseModel.Data(true)
+        );
+        when(restTemplate.getForEntity(anyString(), eq(ResponseModel.class)))
+                .thenReturn(ResponseEntity.ok(response));
     }
 
     @Test
@@ -29,6 +39,30 @@ class HttpAuthorizerTest {
 
         sut.isAuthorized(input);
 
-        verify(restTemplate).getForEntity(HttpAuthorizer.URL, Map.class);
+        verify(restTemplate).getForEntity(HttpAuthorizer.URL, ResponseModel.class);
+    }
+
+    @Test
+    void shouldReturnFalseIfResponseReturnsFalse() {
+        final HttpAuthorizer sut = makeSut();
+        final CreateTransactionInput input = mock(CreateTransactionInput.class);
+        final ResponseModel response = new ResponseModel("success", new ResponseModel.Data(false)
+        );
+        when(restTemplate.getForEntity(anyString(), eq(ResponseModel.class)))
+                .thenReturn(ResponseEntity.ok(response));
+
+        final boolean result = sut.isAuthorized(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnTrueIfResponseReturnsTrue() {
+        final HttpAuthorizer sut = makeSut();
+        final CreateTransactionInput input = mock(CreateTransactionInput.class);
+
+        final boolean result = sut.isAuthorized(input);
+
+        assertTrue(result);
     }
 }
