be9e3fed1e4a025332567ab8200bced2c13cbd86
Fernando Batista <fernandohsbatista@gmail.com>
2025-03-07 10:44:47 -0300

    refactor: clean up some code on DbCreateTransaction

--- src/main/java/com/payments/transactions/data/usecases/DbCreateTransaction.java
+++ src/main/java/com/payments/transactions/data/usecases/DbCreateTransaction.java
@@ -18,6 +18,9 @@ public class DbCreateTransaction implements CreateTransaction {
     private final SendNotification sendNotification;
     private final GetUserByIdRepository getUserByIdRepository;
 
+    private record Users(User payer, User payee) {
+    }
+
     public DbCreateTransaction(
             Authorizer authorizer,
             CreateTransactionRepository createTransactionRepository,
@@ -34,32 +37,25 @@ public class DbCreateTransaction implements CreateTransaction {
 
     @Override
     public Transaction call(CreateTransactionInput input) throws CustomExceptions {
+        final Users users = getUsers(input);
         checkAuthorization(input);
         checkBalance(input);
 
         final Transaction transaction = createTransaction(input);
 
-        final Optional<User> payer = getUserByIdRepository.getById(input.payerId());
-        final Optional<User> payee = getUserByIdRepository.getById(input.payeeId());
-
-        if (payer.isEmpty()) throw new CustomExceptions.PayerNotFound();
-        if (payee.isEmpty()) throw new CustomExceptions.PayeeNotFound();
-
-        final SendNotificationInput payerNotification = new SendNotificationInput(
-                payer.get().email(),
-                "Your transaction has been cleared"
-        );
-
-        final SendNotificationInput payeeNotification = new SendNotificationInput(
-                payee.get().email(),
-        "You have received a transaction"
-        );
-
-
-        sendNotification.send(payerNotification);
-        sendNotification.send(payeeNotification);
+        sendNotifications(users.payer, users.payee);
 
         return transaction;
+    }
+
+    private Users getUsers(CreateTransactionInput input) throws CustomExceptions {
+        final Optional<User> payer = getUserByIdRepository.getById(input.payerId());
+        if (payer.isEmpty()) throw new CustomExceptions.PayerNotFound();
+
+        final Optional<User> payee = getUserByIdRepository.getById(input.payeeId());
+        if (payee.isEmpty()) throw new CustomExceptions.PayeeNotFound();
+
+        return new Users(payer.get(), payee.get());
     }
 
     private void checkAuthorization(CreateTransactionInput input) throws CustomExceptions.NotAuthorized {
@@ -85,5 +81,15 @@ public class DbCreateTransaction implements CreateTransaction {
         if (transaction.isEmpty()) throw new CustomExceptions.PersistanceError();
 
         return transaction.get();
+    }
+
+    private void sendNotifications(User payer, User payee) {
+        sendNotification.send(
+                new SendNotificationInput(payer.email(), "Your transaction has been cleared")
+        );
+
+        sendNotification.send(
+                new SendNotificationInput(payee.email(), "You have received a transaction")
+        );
     }
 }
--- src/main/java/com/payments/transactions/domain/usecases/SendNotificationInput.java
+++ src/main/java/com/payments/transactions/domain/usecases/SendNotificationInput.java
@@ -1,4 +1,9 @@
 package com.payments.transactions.domain.usecases;
 
+import com.payments.users.domain.entities.User;
+
 public record SendNotificationInput(String email, String message) {
+    public static SendNotificationInput fromUser(User user, String message) {
+        return new SendNotificationInput(user.email(), message);
+    }
 }
